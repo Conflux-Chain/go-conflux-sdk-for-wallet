ff7b4bc5db36ce04247764cc3f6f98c024d1cad4
Dayong Wang <dayong@conflux-chain.org>
2020-07-22 17:31:18 +0800

    update go.mod go-sdk version to 0.4.0 optimize examples

--- example/example_converter_txdict/main.go
+++ example/example_converter_txdict/main.go
@@ -2,61 +2,24 @@ package main
 
 import (
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"time"
 
-	sdk "github.com/Conflux-Chain/go-conflux-sdk"
 	walletsdk "github.com/Conflux-Chain/go-conflux-sdk-for-wallet"
+	"github.com/Conflux-Chain/go-conflux-sdk-for-wallet/example/context"
+	exampletypes "github.com/Conflux-Chain/go-conflux-sdk-for-wallet/example/context/types"
 	"github.com/Conflux-Chain/go-conflux-sdk/types"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 var converter *walletsdk.TxDictConverter
 var richClient *walletsdk.RichClient
-
-var contractErc20Address = types.Address("0x8c3da77847b4efa454e6081dd4e898265d1787a2")
-var contractErc777Address = types.Address("0x8726be94d7503b05f1738f026f00e74348c3d3eb")
+var config *exampletypes.Config
 
 func init() {
+	config = context.Prepare()
+	richClient = config.GetRichClient()
 
-	//unlock account
-	am := sdk.NewAccountManager("../keystore")
-	err := am.TimedUnlockDefault("hello", 300*time.Second)
-	if err != nil {
-		panic(err)
-	}
-
-	//init client without retry and excute it
-	//it doesn't work now, you could try later
-	// url := "http://testnet-jsonrpc.conflux-chain.org:12537"
-	url := "http://123.57.45.90:12537"
-
-	client, err := sdk.NewClient(url)
-	if err != nil {
-		panic(err)
-	}
-	client.SetAccountManager(am)
-	config := new(walletsdk.ServerConfig)
-
-	// init rich client
-
-	// main net
-	// config.CfxScanBackendDomain = "47.102.164.229:8885"
-	// config.ContractManagerDomain = "139.196.47.91:8886"
-
-	// public test net (公共测试网)
-	config.CfxScanBackendAddress = "testnet-scantest.confluxscan.io"
-	config.ContractManagerAddress = "testnet-scantest.confluxscan.io/contract-manager"
-
-	// private test net (内部测试网)
-	// config.CfxScanBackendAddress = "101.201.103.131:8885"
-	// config.ContractManagerAddress = "101.201.103.131:8886"
-
-	richClient = walletsdk.NewRichClient(client, config)
-
-	// c := walletsdk.NewTxDictConverter(rc, &walletsdk.TokenReaderByClient{Client: rc.GetClient()})
-	// tr:=walletsdk.TokenReaderByClient
+	var err error
 	converter, err = walletsdk.NewTxDictConverter(richClient)
 	if err != nil {
 		panic(err)
@@ -64,28 +27,30 @@ func init() {
 }
 
 func main() {
+	fmt.Println("=======strat txdict converter example=======")
 	testConvertByTransaction()
 	testConvertByTokenTransferEvent()
 	testConvertByUnsignedTransaction()
 	testConvertByUnsignedTransactionWithoutNetwork()
+	fmt.Println("=======txdict converter example end!=========")
 }
 
 func testConvertByTransaction() {
-	tx, err := richClient.GetClient().GetTransactionByHash("0x86669c1d12e8d0882336b33f8d22d7b4bc7b4c92047a6c6c399c2ebbd16fb28e")
+	tx, err := richClient.GetClient().GetTransactionByHash(config.ERC20Transactions[0])
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("get transaction done: %+v\n\n", jsonFmt(tx))
+	fmt.Printf("get transaction done: %+v\n\n", context.JsonFmt(tx))
 
 	txdict, err := converter.ConvertByTransaction(tx, nil, nil)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Convert tx \n%v\nto txdict done:\n%+v\n\n", jsonFmt(tx), jsonFmt(txdict))
+	fmt.Printf("- Convert tx \n%v\nto txdict done:\n%+v\n\n", context.JsonFmt(tx), context.JsonFmt(txdict))
 }
 
 func testConvertByTokenTransferEvent() {
-	ttes, err := richClient.GetAccountTokenTransfers("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302", &contractErc20Address, 1, 10)
+	ttes, err := richClient.GetAccountTokenTransfers("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302", &config.ERC20Address, 1, 10)
 	if err != nil {
 		panic(err)
 	}
@@ -95,26 +60,26 @@ func testConvertByTokenTransferEvent() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Convert TokenTransferEvent \n%+v\nto txdict result:\n%+v\n\n", jsonFmt(tte), jsonFmt(txdict))
+	fmt.Printf("- Convert TokenTransferEvent \n%+v\nto txdict result:\n%+v\n\n", context.JsonFmt(tte), context.JsonFmt(txdict))
 }
 
 func testConvertByUnsignedTransaction() {
 
-	unsignedTx, err := richClient.CreateSendTokenTransaction(types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302"), types.Address("0x1a6048c1d81190c9a3555d0a06d0699663c4ddf0"), types.NewBigInt(10), &contractErc20Address)
+	unsignedTx, err := richClient.CreateSendTokenTransaction(types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302"), types.Address("0x1a6048c1d81190c9a3555d0a06d0699663c4ddf0"), types.NewBigInt(10), &config.ERC20Address)
 	if err != nil {
 		panic(err)
 	}
 
 	txdictBase := converter.ConvertByUnsignedTransaction(unsignedTx)
-	fmt.Printf("Convert erc20 UnsignedTransaction \n%v\nto TxDictBase done:\n%+v\n\n", jsonFmt(unsignedTx), jsonFmt(txdictBase))
+	fmt.Printf("- Convert erc20 UnsignedTransaction \n%v\nto TxDictBase done:\n%+v\n\n", context.JsonFmt(unsignedTx), context.JsonFmt(txdictBase))
 
-	unsignedTx, err = richClient.CreateSendTokenTransaction(types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302"), types.Address("0x1a6048c1d81190c9a3555d0a06d0699663c4ddf0"), types.NewBigInt(10), &contractErc777Address)
+	unsignedTx, err = richClient.CreateSendTokenTransaction(types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302"), types.Address("0x1a6048c1d81190c9a3555d0a06d0699663c4ddf0"), types.NewBigInt(10), &config.ERC777Address)
 	if err != nil {
 		panic(err)
 	}
 
 	txdictBase = converter.ConvertByUnsignedTransaction(unsignedTx)
-	fmt.Printf("Convert erc777 UnsignedTransaction \n%v\nto TxDictBase done:\n%+v\n\n", jsonFmt(unsignedTx), jsonFmt(txdictBase))
+	fmt.Printf("- Convert erc777 UnsignedTransaction \n%v\nto TxDictBase done:\n%+v\n\n", context.JsonFmt(unsignedTx), context.JsonFmt(txdictBase))
 }
 
 func testConvertByUnsignedTransactionWithoutNetwork() {
@@ -126,21 +91,13 @@ func testConvertByUnsignedTransactionWithoutNetwork() {
 			GasPrice:     types.NewBigInt(0x3b9aca00),
 			Gas:          types.NewBigInt(0x8fb1),
 			Value:        types.NewBigInt(0x0),
-			StorageLimit: types.NewBigInt(0x40),
-			EpochHeight:  types.NewBigInt(0x1eb1ea),
-			ChainID:      types.NewBigInt(0x1)},
+			StorageLimit: types.NewUint64(0x40),
+			EpochHeight:  types.NewUint64(0x1eb1ea),
+			ChainID:      types.NewUint(0x1)},
 		To:   types.NewAddress("0x8c3da77847b4efa454e6081dd4e898265d1787a2"),
 		Data: hexutil.Bytes(data),
 	}
 
 	txdictBase := converter.ConvertByUnsignedTransaction(unsignedTx)
-	fmt.Printf("Convert erc20 UnsignedTransaction \n%v\nto TxDictBase done:\n%+v\n\n", jsonFmt(unsignedTx), jsonFmt(txdictBase))
-}
-
-func jsonFmt(input interface{}) string {
-	j, err := json.Marshal(input)
-	if err != nil {
-		panic(err)
-	}
-	return string(j)
+	fmt.Printf("- Convert erc20 UnsignedTransaction \n%v\nto TxDictBase done:\n%+v\n\n", context.JsonFmt(unsignedTx), context.JsonFmt(txdictBase))
 }
--- example/example_richclient/main.go
+++ example/example_richclient/main.go
@@ -1,59 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
 	"math/big"
 	"time"
 
-	sdk "github.com/Conflux-Chain/go-conflux-sdk"
+	"github.com/BurntSushi/toml"
 	richsdk "github.com/Conflux-Chain/go-conflux-sdk-for-wallet"
+	context "github.com/Conflux-Chain/go-conflux-sdk-for-wallet/example/context"
+	exampletypes "github.com/Conflux-Chain/go-conflux-sdk-for-wallet/example/context/types"
 	"github.com/Conflux-Chain/go-conflux-sdk/types"
 )
 
 var rc *richsdk.RichClient
-var contractErc20Address = types.Address("0x8c3da77847b4efa454e6081dd4e898265d1787a2")
-var contractErc777Address = types.Address("0x8726be94d7503b05f1738f026f00e74348c3d3eb")
+var config *exampletypes.Config
 
 func init() {
-
-	//unlock account
-	am := sdk.NewAccountManager("../keystore")
-	err := am.TimedUnlockDefault("hello", 300*time.Second)
-	if err != nil {
-		panic(err)
-	}
-
-	//init client without retry and excute it
-	//it doesn't work now, you could try later
-	// url := "http://testnet-jsonrpc.conflux-chain.org:12537"
-	url := "http://123.57.45.90:12537"
-
-	client, err := sdk.NewClient(url)
-	if err != nil {
-		panic(err)
-	}
-	client.SetAccountManager(am)
-	config := new(richsdk.ServerConfig)
-
-	// init rich client
-
-	// main net
-	// config.CfxScanBackendDomain = "47.102.164.229:8885"
-	// config.ContractManagerDomain = "139.196.47.91:8886"
-
-	// public test net (公共测试网)
-	config.CfxScanBackendAddress = "testnet-scantest.confluxscan.io"
-	config.ContractManagerAddress = "testnet-scantest.confluxscan.io/contract-manager"
-
-	// private test net (内部测试网)
-	// config.CfxScanBackendAddress = "101.201.103.131:8885"
-	// config.ContractManagerAddress = "101.201.103.131:8886"
-
-	rc = richsdk.NewRichClient(client, config)
+	config = context.Prepare()
+	rc = config.GetRichClient()
 }
 
 func main() {
+	fmt.Println("\n=======start rich client example=======\n")
 	testGetAccountTokenTransfers()
 	testGetAccountTokens()
 	testCreateSendTokenTransaction()
@@ -61,40 +30,48 @@ func main() {
 	testGetTxDictByTxHash()
 	testGetContractInfo()
 	testGetTransactionsFromPool()
+	fmt.Println("\n=======rich client example done=======\n")
+}
+
+func readConfig() {
+	var path string = "../config.toml"
+	if _, err := toml.DecodeFile(path, &config); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func testGetAccountTokenTransfers() {
 	start := time.Now()
 	from := types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302")
-	token := contractErc20Address
+	token := config.ERC20Address
 	tteList, err := rc.GetAccountTokenTransfers(from, &token, 1, 50)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("get account %v token %v transers done, token transfer list is:\n%+v\nused time:%v\n\n",
-		from, token, jsonFmt(tteList), time.Now().Sub(start))
+	fmt.Printf("- get account %v token %v transers done, token transfer list is:\n%+v\nused time:%v\n\n",
+		from, token, context.JsonFmt(tteList), time.Now().Sub(start))
 
 	start = time.Now()
 	tteList, err = rc.GetAccountTokenTransfers(from, nil, 1, 50)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("get account %v main coin transers done, token transfer list is:\n%+v\nused time:%v\n\n",
-		from, jsonFmt(tteList), time.Now().Sub(start))
+	fmt.Printf("- get account %v CFX coin transers done, token transfer list is:\n%+v\nused time:%v\n\n",
+		from, context.JsonFmt(tteList), time.Now().Sub(start))
 }
 
 func testCreateSendTokenTransaction() {
-	tx, err := rc.CreateSendTokenTransaction(types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302"), types.Address("0x1a6048c1d81190c9a3555d0a06d0699663c4ddf0"), types.NewBigInt(10), &contractErc20Address)
+	tx, err := rc.CreateSendTokenTransaction(types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302"), types.Address("0x1a6048c1d81190c9a3555d0a06d0699663c4ddf0"), types.NewBigInt(10), &config.ERC20Address)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("create send erc20 token tx:%+v\n\n", jsonFmt(tx))
+	fmt.Printf("- create send erc20 token tx:%+v\n\n", context.JsonFmt(tx))
 
-	tx, err = rc.CreateSendTokenTransaction(types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302"), types.Address("0x1a6048c1d81190c9a3555d0a06d0699663c4ddf0"), types.NewBigInt(10), &contractErc777Address)
+	tx, err = rc.CreateSendTokenTransaction(types.Address("0x19f4bcf113e0b896d9b34294fd3da86b4adf0302"), types.Address("0x1a6048c1d81190c9a3555d0a06d0699663c4ddf0"), types.NewBigInt(10), &config.ERC777Address)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("create send erc777 token tx:%+v\n\n", jsonFmt(tx))
+	fmt.Printf("- create send erc777 token tx:%+v\n\n", context.JsonFmt(tx))
 }
 
 func testGetAccountTokens() {
@@ -102,7 +79,7 @@ func testGetAccountTokens() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("address has tokens:\n%+v\n\n", jsonFmt(ts))
+	fmt.Printf("- address has tokens:\n%+v\n\n", context.JsonFmt(ts))
 
 }
 
@@ -111,7 +88,7 @@ func testGetTransactionsFromPool() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("txs in pool is:\n%+v\n\n", jsonFmt(txs))
+	fmt.Printf("- txs in pool is:\n%+v\n\n", context.JsonFmt(txs))
 }
 
 func testGetTransactionsByEpoch() {
@@ -122,30 +99,22 @@ func testGetTransactionsByEpoch() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("get txdicts of epoch %v done, txidcts is\n%+v, used time: %v\n\n", epochNum, jsonFmt(txdicts), time.Now().Sub(start))
+	fmt.Printf("- get txdicts of epoch %v done, txidcts is\n%+v, used time: %v\n\n", epochNum, context.JsonFmt(txdicts), time.Now().Sub(start))
 }
 
 func testGetTxDictByTxHash() {
-	hash := types.Hash("0xeb34792e27e00c081843e308de428cf792631524a3072162ce1f4bf63ea0e843")
+	hash := types.Hash(config.ERC20Transactions[0])
 	txdict, err := rc.GetTxDictByTxHash(hash)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("get txdict by txhash done\n%+v\n\n", jsonFmt(txdict))
+	fmt.Printf("- get txdict by txhash done\n%+v\n\n", context.JsonFmt(txdict))
 }
 
 func testGetContractInfo() {
-	tokenInfo, err := rc.GetContractInfo(contractErc20Address, true)
+	tokenInfo, err := rc.GetContractInfo(config.ERC20Address, true)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("get token info of %s done\n%v\n\n", contractErc20Address, jsonFmt(tokenInfo))
-}
-
-func jsonFmt(input interface{}) string {
-	j, err := json.Marshal(input)
-	if err != nil {
-		panic(err)
-	}
-	return string(j)
+	fmt.Printf("- get token info of %s done\n%v\n\n", config.ERC20Address, context.JsonFmt(tokenInfo))
 }
